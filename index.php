8e2461628d1a622221c21cf02a1dc54c3868a6fb
akbarhashmi <akbar6127@gmail.com>
2018-02-13 21:02:39 +0530

    Add initial structure

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+session_start();
+
+$_SESSION['user'] = (isset($_GET['user']) === true) ? (int)$_GET['user'] : 0;
+
+echo $_SESSION['user'] ;	
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		AJAX Chat
+	</title>
+	<link rel="stylesheet" type = "text/css" href="./css/styles.css">
+</head>
+<body>
+    
+	<div class="chat"> 
+		<div class="messages">
+            <div class="message">
+                <a href="#">Akbar</a> says:
+                <p>The message will display here</p>
+            </div>
+        </div>
+		<textarea class="entry" placeholder="Type here"></textarea>
+	</div>
+	<script src="http://code.jquery.com/jquery-3.3.1.js"></script>
+	<script src="js/chat.js" ></script>
+</body>
+</html>
