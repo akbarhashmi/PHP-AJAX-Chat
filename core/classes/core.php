c301b364f057720be117480ce7c41311dec7a7a4
akbarhashmi <akbar6127@gmail.com>
2018-02-14 23:46:25 +0530

    Modified core.php

---
+++ core/classes/core.php
@@ -0,0 +1,27 @@
+<?php
+
+class Core{
+
+	protected $db, $result; //Protected so that they can be accessed only by inheritance 
+	private $rows;
+
+	public function __construct(){
+		$this->db= new mysqli('chatdb','root','root','localhost');
+
+
+	}
+
+	public function query($sql){
+		$this->result = $this->db->query($sql);
+
+	}
+
+	public function rows(){ //loops through rows returned fro the query
+		for(x=1; x <= $this->db->affected_rows; x++){
+			$this->rows[] = $this->result>fetch_assoc();
+		}
+
+		return $this->rows;
+
+	}
+}
---
+++ core/init.php
@@ -0,0 +1,8 @@
+<?php
+
+start_session();
+define('LOGGED_IN',true);
+
+require 'classes/chat.php';
+require 'classes/chat.php';
+
